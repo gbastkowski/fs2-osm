533cdf60f94a4abe37b6cfae9d65d501a8cca5fe
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-08-29 13:44:41 +0200

    Upgrade dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,21 +3,21 @@ import sbt._
 object Versions {
   val doobie              = "1.0.0-RC5"
   val embeddedPostgres    = "1.1.0"
-  val fs2                 = "3.10.2"
+  val fs2                 = "3.11.0"
   val http4s              = "0.23.26"
   val log4cats            = "2.6.0"
   val log4j               = "2.23.1"
   val log4jScala          = "13.1.0"
   val odin                = "0.13.0"
-  val opentelemetry       = "1.40.0"
-  val opentelemetryAlpha  = "1.40.0-alpha"
-  val otel4s              = "0.8.0"
+  val opentelemetry       = "1.41.0"
+  val opentelemetryAlpha  = "1.41.0-alpha"
+  val otel4s              = "0.8.1"
   val postgisJdbc         = "2.5.1"
   val pureconfig          = "0.17.7"
   val scalaLogging        = "3.9.4"
   val scala               = "3.4.2"
   val scopt               = "4.1.0"
-  val sttp                = "3.9.7"
+  val sttp                = "3.9.8"
   val weaver              = "0.8.4"
 }
 
@@ -53,10 +53,10 @@ object Dependencies {
   val otelJava = Seq(
     "io.opentelemetry"                  % "opentelemetry-exporter-logging"            % Versions.opentelemetry,
     "io.opentelemetry"                  % "opentelemetry-exporter-otlp"               % Versions.opentelemetry,
-    "io.opentelemetry"                  % "opentelemetry-exporter-prometheus"         % "1.40.0-alpha",
-    "io.opentelemetry.javaagent"        % "opentelemetry-javaagent"                   % "2.6.0",
+    "io.opentelemetry"                  % "opentelemetry-exporter-prometheus"         % Versions.opentelemetryAlpha,
+    "io.opentelemetry.javaagent"        % "opentelemetry-javaagent"                   % "2.7.0",
     "io.opentelemetry"                  % "opentelemetry-sdk-extension-autoconfigure" % Versions.opentelemetry,
-    "io.opentelemetry.instrumentation"  % "opentelemetry-runtime-telemetry-java17"    % "2.6.0-alpha",
+    "io.opentelemetry.instrumentation"  % "opentelemetry-runtime-telemetry-java17"    % "2.7.0-alpha",
     "org.typelevel"                    %% "otel4s-oteljava"                           % Versions.otel4s)
 
   val pureconfig = Seq(
